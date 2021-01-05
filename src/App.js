483de4905448c1b6debf07768004882fc1495219
gauravmadan583 <54912404+gauravmadan583@users.noreply.github.com>
2021-01-05 23:13:34 +0530

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
 import { Component } from 'react';
 
 class App extends Component {
--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,59 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
-    date(d) {
-        var yr=d.slice(0, 4);
-        var mn=d.slice(5, 7);
-        var dt=d.slice(8, 10);
-        let mp = new Map();
-        mp["01"] = "Jan";
-        mp["02"] = "Feb";
-        mp["03"] = "Mar";
-        mp["04"] = "Apr";
-        mp["05"] = "May";
-        mp["06"] = "Jun";
-        mp["07"] = "Jul";
-        mp["08"] = "Aug";
-        mp["09"] = "Sep";
-        mp["10"] = "Oct";
-        mp["11"] = "Nov";
-        mp["12"] = "Dec";
-    
-        var ans = mp[mn] + " " + dt + ", " + yr;
-        return ans;
-    }
-    render() {
+function date(d) {
+    var yr=d.slice(0, 4);
+    var mn=d.slice(5, 7);
+    var dt=d.slice(8, 10);
+    let mp = new Map();
+    mp["01"] = "Jan";
+    mp["02"] = "Feb";
+    mp["03"] = "Mar";
+    mp["04"] = "Apr";
+    mp["05"] = "May";
+    mp["06"] = "Jun";
+    mp["07"] = "Jul";
+    mp["08"] = "Aug";
+    mp["09"] = "Sep";
+    mp["10"] = "Oct";
+    mp["11"] = "Nov";
+    mp["12"] = "Dec";
 
-        var comments = this.props.dish.comments.map((comment) => {
-            return(
-                <li key={comment.id} className="unlisted">
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author}, {this.date(comment.date)}</p>
-                </li>
-            );
-        })
-        
+    var ans = mp[mn] + " " + dt + ", " + yr;
+    return ans;
+}
+
+    
+
+const DishDetail = (props) => {
+    var comments = props.dish.comments.map((comment) => {
         return(
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" object src={this.props.dish.image}/>
-                        <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul>
-                        {comments}
-                    </ul>
-                </div>
-            </div>
+            <li key={comment.id} className="unlisted">
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, {date(comment.date)}</p>
+            </li>
         );
-    }
-};
+    })
+    
+    return(
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" object src={props.dish.image}/>
+                    <CardBody>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                        <CardText>{props.dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul>
+                    {comments}
+                </ul>
+            </div>
+        </div>
+    );
+}
 
 export default DishDetail;
